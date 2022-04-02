04fad6b7ceac3a24a76079e28faa043218d86bac
catafang <catafang.codeuser@gmail.com>
2022-04-02 16:44:51 +0800

    ＪＳＯＮ載入、讀取、渲染

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const express = require('express')
 const port = 3000
 const app = express()
 const exphbs = require('express-handlebars')
+const restaurantList = require ('./restaurants.json') //載入json
+
 
 app.get('/', (req,res)=>{
-  //res.send (`successed express web app `)
-  res.render('index')
+  // const restaurantOne ={
+  //     "id": 1,
+  //     "name": "Sababa 沙巴巴中東美食",
+  //     "name_en": "Sababa Pita Bar",
+  //     "category": "中東料理",
+  //     "image": "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg",
+  //     "location": "台北市羅斯福路三段 283 巷 17 號",
+  //     "phone": "02 2363 8009",
+  //     "google_map": "https://goo.gl/maps/BJdmLuVdDbw",
+  //     "rating": 4.1,
+  //     "description": "沙巴巴批塔是台灣第一家純手工批塔專賣店,只選用最新鮮的頂級原料,以及道地的中東家傳配方。"
+  // }
+  res.render('index', {restaurants:restaurantList.results})  //讀取與渲染 Json資料)
 })
 
 app.engine ('handlebars', exphbs({defaultLayout:"main"}))
